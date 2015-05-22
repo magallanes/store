580e1cde6a8b564a9f95fbbbaa319dc5a7aadf26
System Administrator <root@MacBook-Pro-de-Fernando.local>
2015-05-22 09:01:04 -0500

    WooCommerce funcionando (Sin editar) 22 May

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'wordpres');
 
 /** MySQL database username */
-define('DB_USER', 'tienda2');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Maquinadefuego69');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
